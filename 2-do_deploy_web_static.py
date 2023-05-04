b05ad5e7aff9a61d1d7f9e477229b8092ea89258
berekettena <tena21bereket@gmail.com>
2023-05-04 18:43:00 +0300

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:
    Prototype: def do_deploy(archive_path):
    Returns False if the file at the path archive_path doesn’t exist
    The script should take the following steps:
    Upload the archive to the /tmp/ directory of the web server
    Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
    Delete the archive from the web server
    Delete the symbolic link /data/web_static/current from the web server
    Create a new the symbolic link /data/web_static/current on the web server, linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
    All remote commands must be executed on your both web servers (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
    Returns True if all operations have been done correctly, otherwise returns False
    You must use this script to deploy it on your servers: xx-web-01 and xx-web-02
    In the following example, the SSH key and the username used for accessing to the server are passed in the command line. Of course, you could define them as Fabric environment variables (ex: env.user =...)
    
    Disclaimer: commands execute by Fabric displayed below are linked to the way we implemented the archive function do_pack - like the mv command - depending of your implementation of it, you may don’t need it

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Fabric script to deploy web_static archive to web servers
+"""
+
+
+from fabric.api import *
+import os
+
+
+env.user = 'ubuntu'
+env.hosts = ['52.91.178.133', '54.160.69.6']
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to the web servers
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    filename = os.path.basename(archive_path)
+    name = os.path.splitext(filename)[0]
+
+    try:
+        """ Upload the archive to the /tmp/ directory of the web server"""
+        put(archive_path, '/tmp/')
+
+        """ Create the directory to uncompress the archive"""
+        run('mkdir -p /data/web_static/releases/{}/'.format(name))
+
+        """ Uncompress the archive to the folder"""
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(filename, name))
+
+        """ Delete the archive from the web server"""
+        run('rm /tmp/{}'.format(filename))
+
+        """ Move the files to the new folder and remove the old folder"""
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(name, name))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(name))
+
+        """ Delete the symbolic link /data/web_static/current from the web"""
+        run('rm -rf /data/web_static/current')
+
+        """ Create a new the symbolic link /data/web_static/current"""
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(name))
+
+        print('New version deployed!')
+        return True
+
+    except Exception:
+        return False
