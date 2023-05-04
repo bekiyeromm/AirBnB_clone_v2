e57166a0c492360606d340096d51aaad6210cae4
berekettena <tena21bereket@gmail.com>
2023-05-04 10:29:10 +0300

    Write a Bash script that sets up your web servers for the deployment of web_static. It must Install Nginx if it not already installed Create the folder /data/ if it doesn’t already exist Create the folder /data/web_static/ if it doesn’t already exist Create the folder /data/web_static/releases/ if it doesn’t already exist Create the folder /data/web_static/shared/ if it doesn’t already exist Create the folder /data/web_static/releases/test/ if it doesn’t already exist Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration) Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran. Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group. Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration: Use alias inside your Nginx configuration Your program should always exit successfully. Don’t forget to run your script on both of your web servers.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,11 @@
 # a script that sets up your web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get install -y nginx
-sudo mkdir -p /data/web_static/releases/test
+sudo service nginx start
 sudo mkdir -p /data/web_static/shared/
-echo "hello world" | sudo tee /data/web_static/releases/test/index.html>/dev/null
+echo "hello world" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo rm -rf /data/web_static/current
 ls -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubunt /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
